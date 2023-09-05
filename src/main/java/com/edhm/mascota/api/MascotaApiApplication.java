75bd240220edf407fe5b608f954d74a2394d5c3e
Edgar Huerta <ed.huerta.m@gmail.com>
2023-09-04 19:58:19 -0600

    Eureka-Client implementation service mascot

--- src/main/java/com/edhm/mascota/api/MascotaApiApplication.java
+++ src/main/java/com/edhm/mascota/api/MascotaApiApplication.java
@@ -2,8 +2,10 @@ package com.edhm.mascota.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MascotaApiApplication {
 
 	public static void main(String[] args) {
